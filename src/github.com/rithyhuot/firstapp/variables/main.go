d8bd8321e20a636b7698fe2b6feecf8cd6521c27
Rithy Huot <rithybhuot@gmail.com>
2020-03-11 23:34:54 -0400

    add summary for variables

--- src/github.com/rithyhuot/firstapp/variables/main.go
+++ src/github.com/rithyhuot/firstapp/variables/main.go
@@ -62,3 +62,25 @@ func main() {
 
 	fmt.Printf("%v, %T", j, j)
 }
+
+/*
+ * SUMMARY
+ *  Variable declaration
+ *   var foo int  - less common
+ *   var foo int = 42 - common
+ *   foo := 42 - more common
+ *  Can't redeclare variables, but can shadow them
+ *  All variables must be used
+ *  Visibility
+ *   lower case first letter for package scope
+ *   upper case first letter to export
+ *   no private scope
+ *  Naming conventions
+ *   Pascal or camelCase
+ *     Capitalize acronyms (HTTP, URL)
+ *   As short as reasonable
+ *     longer names for longer lives
+ *  Type conversions
+ *    destinationType(variable)
+ *    use strconv package for strings
+ */
